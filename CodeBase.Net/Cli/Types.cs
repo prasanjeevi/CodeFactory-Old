3e8c3cdcc13a6e6f9badfe7d9ff462fa66b15ff2
prasanjeevi <prasanjeevi@gmail.com>
2018-07-06 13:21:27 +0530

    add E-Notation convertion

--- CodeBase.Net/Cli/Types.cs
+++ CodeBase.Net/Cli/Types.cs
@@ -5,23 +5,27 @@ namespace Cli
 {
     class Types
     {
-      const string FormatENotationToNormal = "#,##0";
-      const string FormatENotationToNormalWithComma = "#,##0";
-
       internal void Print()
-        {
-            WriteLine($"| Name | MinValue | MaxValue | Type |");
-            WriteLine($"| --- | --- | --- | --- |");
-            WriteLine($"| sbyte | {sbyte.MinValue} | {sbyte.MaxValue} | {typeof(sbyte)} |");
-            WriteLine($"| byte | {byte.MinValue} | {byte.MaxValue} | {typeof(byte)} |");
-            WriteLine($"| short | {short.MinValue} | {short.MaxValue} | {typeof(short)} |");
-            WriteLine($"| ushort | {ushort.MinValue} | {ushort.MaxValue} | {typeof(ushort)} |");
-            WriteLine($"| int | {int.MinValue} | {int.MaxValue} | {typeof(int)} |");
-            WriteLine($"| uint | {uint.MinValue} | {uint.MaxValue} | {typeof(uint)} |");
-            WriteLine($"| long | {long.MinValue} | {long.MaxValue} | {typeof(long)} |");
-            WriteLine($"| ulong | {ulong.MinValue} | {ulong.MaxValue} | {typeof(ulong)} |");
-            WriteLine($"| float | {float.MinValue} | {float.MaxValue} | {typeof(float)} |");
-            WriteLine($"| decimal | {decimal.MinValue} | {decimal.MaxValue} | {typeof(decimal)} |");
-        }
+      {
+          WriteLine($"| Name | MinValue | MaxValue | Type |");
+          WriteLine($"| --- | --- | --- | --- |");
+          WriteLine($"| sbyte | {sbyte.MinValue} | {sbyte.MaxValue} | {typeof(sbyte)} |");
+          WriteLine($"| byte | {byte.MinValue} | {byte.MaxValue} | {typeof(byte)} |");
+          WriteLine($"| short | {short.MinValue} | {short.MaxValue} | {typeof(short)} |");
+          WriteLine($"| ushort | {ushort.MinValue} | {ushort.MaxValue} | {typeof(ushort)} |");
+          WriteLine($"| int | {int.MinValue} | {int.MaxValue} | {typeof(int)} |");
+          WriteLine($"| uint | {uint.MinValue} | {uint.MaxValue} | {typeof(uint)} |");
+          WriteLine($"| long | {long.MinValue} | {long.MaxValue} | {typeof(long)} |");
+          WriteLine($"| ulong | {ulong.MinValue} | {ulong.MaxValue} | {typeof(ulong)} |");
+          WriteLine($"| float | {float.MinValue} | {float.MaxValue} | {typeof(float)} |");
+          WriteLine($"| decimal | {decimal.MinValue} | {decimal.MaxValue} | {typeof(decimal)} |");
+          PrintWithoutENotation(float.MinValue);
+          PrintWithoutENotation(float.MaxValue);
+      }
+
+      internal void PrintWithoutENotation(object o)
+      {
+        WriteLine($"{o} = {o:#}");
+      }
     }
 }
